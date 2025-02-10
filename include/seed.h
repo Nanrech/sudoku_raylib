ad1e56cbf366887510965473e79d4e9c63050a30
Nan R <88463477+Nanrech@users.noreply.github.com>
2025-02-10 14:07:40 +0100

    Update seed.h

--- include/seed.h
+++ include/seed.h
@@ -3,6 +3,7 @@
 
 #include "constants.h"
 
+// I don't like the way this looks
 const char seedData[SEED_AMOUNT * SEED_LEN * 2 + 1] = "g.ca..fi.ida.f........ch.a.dgf....bhb..f.i..da......e.hi..d..f.c.g.ife...b..ea..."
                                                       "ghcabdfieidagfebhcefbichdagdgfeacibhbehfgiacdacidhbgefhiebdgcfacaghifedbfbdceahgi"
                                                       "..b.ci.h......eg..a..hg..b..a.g.b..d....hc..afhi.dacg..bg.a....hdf.e..a....cf..d."
@@ -13,6 +14,5 @@ const char seedData[SEED_AMOUNT * SEED_LEN * 2 + 1] = "g.ca..fi.ida.f........ch.
                                                       "fahbigdceeciadhfgbdgbecfiahhdcgbiaefaifcedbhggbefhacidifahgbedcchdifegbabegdachfi"
                                                       ".fd..b...b.ced...ig.....f..d....h.f...hi....a.i.f..hebic.d....he...a.d..ad....egc"
                                                       "hfdgibacebacedfghigeichafbddbeachifgfghibecdaciafgdhebicfdegbahehgbacdifadbhfiegc";
-
 
 #endif // SEED_H
